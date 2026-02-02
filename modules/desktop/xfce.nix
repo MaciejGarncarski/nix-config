b821d91a7c07b6e82cf8cf697781292d8813f8f8
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-02-02 18:07:35 +0100

    fix: xfce

--- modules/desktop/xfce.nix
+++ modules/desktop/xfce.nix
@@ -3,7 +3,7 @@
   services.xserver.enable = true;
 
   services.displayManager.lightdm.enable = true;
-  services.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
   environment.systemPackages = with pkgs; [
     xfce.thunar
